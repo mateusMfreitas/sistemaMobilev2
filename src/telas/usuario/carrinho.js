e992de30c9b7aaef51ef24468959661e29744b52
Mateus Martins Freitas <mateusmfreitas12@gmail.com>
2023-10-05 19:47:53 -0300

    Adicionado funcionamento do carrinho

--- src/telas/usuario/carrinho.js
+++ src/telas/usuario/carrinho.js
@@ -1,12 +1,35 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, FlatList } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 export default function Carrinho({ navigation }) {
+  const [ids, setIds] = useState([]);
+  useEffect(() => {
+    getCarrinho();
+  }, []);
+  async function getCarrinho(){
+    try{
+    const carrinho = await AsyncStorage.getItem("carrinho");
+    const carrinhoObj = JSON.parse(carrinho);
+    if (carrinhoObj.id !== null) {
+      setIds(carrinhoObj.id.split(','));
+    }
+  }catch{
+      console.log('Erro ao buscar os IDs');
+    }
+  }
+
+
  return( 
-  <View>
-      <Text>Configuracoeeeeeesssssssss</Text>
-    </View>
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <FlatList
+        data={ids}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <Text>{item}</Text>}
+      />
+      <Button title="Finalizar Compra"></Button>
+  </View>
  );
 
 }
